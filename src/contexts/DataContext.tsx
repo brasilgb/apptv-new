4c2485ebcd8be4043c7195b5f581554ac8225615
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-03-20 10:36:39 -0300

    Push update

--- src/contexts/DataContext.tsx
+++ src/contexts/DataContext.tsx
@@ -11,28 +11,6 @@ export const DataProvider = ({ children }: DataProps) => {
     const [dataFaturamento, setDataFaturamento] = useState<any>([]);
     const [dataGrafico, setDataGrafico] = useState<any>([]);
 
-    useEffect(() => {
-        const getDataFaturamento =  () => {
-            // const file = await fs.readFile(process.cwd() + '/public/data/faturamento-painel-tv.json', 'utf8');
-            // const data = JSON.parse(file);
-            fetch('/public/data/faturamento-painel-tv.json')
-            .then((res) => res.json())
-            .then((data) => {
-                setDataFaturamento(data)
-            })
-        };
-        getDataFaturamento();
-    }, [fs]);
-
-    // useEffect(() => {
-    //     const getDataGrafico = async () => {
-    //         const file = await fs.readFile(process.cwd() + '/public/data/grafico-painel-tv.json', 'utf8');
-    //         const data = JSON.parse(file);
-    //         setDataGrafico(data);
-    //     };
-    //     getDataGrafico();
-    // }, [fs]);
-
     return (
         <DataContext.Provider value={{
             dataFaturamento,
