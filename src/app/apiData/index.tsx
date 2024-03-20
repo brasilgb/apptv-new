da622aa567df5bd3774e930f31047cbfdc4a7e7c
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-03-20 17:22:52 -0300

    Push

--- src/app/apiData/index.tsx
+++ src/app/apiData/index.tsx
@@ -2,14 +2,14 @@ import { promises as fs } from 'fs';
 
 export const getDataFaturamento = async () => {
   // const file:any = await fs.readFile(process.cwd() + '/src/lib/faturamento-painel-tv.json');
-  const file: any = await fs.readFile('/mnt/apptv/faturamento-painel-tv.json');
+  const file: any = await fs.readFile('/mnt/apptv/PainelTV/faturamento-painel-tv.json');
   const data = JSON.parse(file);
   return data;
 };
 
 export const getDataGrafico = async () => {
   // const file = await fs.readFile(process.cwd() + '/src/lib/grafico-painel-tv.json', 'utf8');
-  const file = await fs.readFile('/mnt/apptv/grafico-painel-tv.json', 'utf8');
+  const file = await fs.readFile('/mnt/apptv/PainelTV/grafico-painel-tv.json', 'utf8');
   const data = JSON.parse(file);
   return data;
 };
