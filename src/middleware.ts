beae8a7c79ba4011ca02461f3d4a386294f36463
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-06-14 13:45:40 -0300

    Push

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,38 @@ import { NextRequest, NextResponse } from 'next/server';
 import cookie from 'cookie';
 
 export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
   const cookies = req.headers.get('cookie') || '';
   const { portal_access } = cookie.parse(cookies);
 
   if (!portal_access) {
-    return NextResponse.redirect(new URL('/acesso-negado', req.url));
+    console.log('No portal access cookie found, redirecting to /not-found');
+    return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  const user = JSON.parse(portal_access);
-  const allowedPaths = user.folders.map((folder: { path: string }) => folder.path);
+  let user;
+  try {
+    user = JSON.parse(portal_access);
+  } catch (e) {
+    console.log('Failed to parse portal_access cookie, redirecting to /not-found');
+    return NextResponse.redirect(new URL(`/${process.env.NEXT_PUBLIC_APP_ID}/not-found`, req.url));
+  }
 
-  const isPathAllowed = allowedPaths.some((allowedPath: string) => pathname.startsWith(`${allowedPath}`));
+  const allowedPaths = user.folders.map((folder: { path: string }) => folder.path);
+  console.log('Allowed Paths:', allowedPaths);
+
+  // Check if the full pathname is allowed
+  const isPathAllowed = allowedPaths.some((allowedPath: string) => process.env.NEXT_PUBLIC_APP_ID === allowedPath);
+  console.log('Is Path Allowed:', isPathAllowed);
 
   if (!isPathAllowed) {
-    return NextResponse.redirect(new URL('/acesso-negado', req.url));
+    console.log('Path not allowed, redirecting to /not-found');
+    return NextResponse.redirect(new URL(`/${process.env.NEXT_PUBLIC_APP_ID}/not-found`, req.url));
   }
 
+  console.log('Tem permissão, seguirá com a requisição...', process.env.NEXT_PUBLIC_APP_ID)
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|logotipo.svg).*)'],
 };
